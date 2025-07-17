bdd64bb295b5abd40b1184b1e66da297cc87992f
Rubén Laguna <ruben.laguna@gmail.com>
2025-07-17 23:37:39 +0200

    Bump version

--- check_version_bump.sh
+++ check_version_bump.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -uo pipefail
 
-git show origin/main:VERSION.txt >/dev/null
+VERSION=$(git show origin/main:VERSION.txt)
 retVal=$?
 if [ $retVal -ne 0 ]; then
     echo "VERSION.txt does not exist in origin/main yet so we skip the test"
     exit 0
 fi
 
+pysemver bump patch "${VERSION}" >VERSION.txt || exit 1
+exit 0
 
-exit 1
+
